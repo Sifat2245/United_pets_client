ea7d7acb01de10cb78d3dae8569dd5f6a0301691
Sifat2245 <Sifat2245@users.noreply.github.com>
2025-07-08 21:09:52 +0600

    initialize the project. and set up necessary packages to start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="">
+      <Button className={'bg-blue-600 hover:bg-red-500'}>Click me</Button>
+    </div>
+
+    
+  )
+}
+
+export default App
